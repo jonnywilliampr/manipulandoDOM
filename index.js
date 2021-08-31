6cd2824016766a02575d7d38a06274df8e5fe7bb
Jonny William <jonnywilliampr@gmail.com>
2021-08-31 19:38:13 -0300

    Feat: Manipulando o DOM da página.

---
+++ index.js
@@ -0,0 +1,15 @@
+// let link = document.createElement('a');
+// link.setAttribute('href', 'https://google.com')
+
+// let conteudo = document.createTextNode("Acessar ao Google");
+// link.appendChild(conteudo);
+
+// let container = document.querySelector('#app');
+// container.appendChild(link);
+
+
+// link.style.textDecoration ='none';
+// link.style.color = '#ffffff';
+
+let box = document.querySelector('.box');
+console.log(box);
