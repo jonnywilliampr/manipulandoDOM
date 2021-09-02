cf2b3471de9f3f7b08889fa4df12f4c93ccf449f
Jonny William <jonnywilliampr@gmail.com>
2021-09-01 22:50:36 -0300

    feat: aplicando evento por keydown

--- index.js
+++ index.js
@@ -11,5 +11,22 @@
 // link.style.textDecoration ='none';
 // link.style.color = '#ffffff';
 
+// let box = document.querySelector('.box');
+// console.log(box);
+
 let box = document.querySelector('.box');
-console.log(box);
+
+function callback(event){
+    // console.log(event.currentTarget);
+    // console.log(event.target);
+    // console.log(event.path);
+    if(event.key == 'r'){
+        box.classList.toggle('red');
+    }else if(event.key == 'b'){
+        box.classList.toggle('blue');
+    }else{
+        box.classList.toggle('green');
+    }
+}
+
+window.addEventListener('keydown', callback);
