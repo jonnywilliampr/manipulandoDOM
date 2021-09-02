71f2434f32d526d0d545cf7bd7954543d342a951
Jonny William <jonnywilliampr@gmail.com>
2021-09-02 17:57:38 -0300

    feat: desenvolvendo um accordion button

--- index.js
+++ index.js
@@ -14,19 +14,38 @@
 // let box = document.querySelector('.box');
 // console.log(box);
 
-let box = document.querySelector('.box');
+// let box = document.querySelector('.box');
 
-function callback(event){
-    // console.log(event.currentTarget);
-    // console.log(event.target);
-    // console.log(event.path);
-    if(event.key == 'r'){
-        box.classList.toggle('red');
-    }else if(event.key == 'b'){
-        box.classList.toggle('blue');
-    }else{
-        box.classList.toggle('green');
-    }
-}
+// function callback(event){
+//     // console.log(event.currentTarget);
+//     // console.log(event.target);
+//     // console.log(event.path);
+//     if(event.key == 'r'){
+//         box.classList.toggle('red');
+//     }else if(event.key == 'b'){
+//         box.classList.toggle('blue');
+//     }else{
+//         box.classList.toggle('green');
+//     }
+// }
 
-window.addEventListener('keydown', callback);
+// window.addEventListener('keydown', callback);
+
+// Acordion List
+
+document.querySelectorAll('.accordion-content').forEach(item =>{
+    item.style.maxHeight = 0;
+});
+
+document.querySelectorAll('.accordion-button').forEach(button =>{
+    button.addEventListener('click', ()=>{
+        const accordionContent = button.nextElementSibling;
+        button.classList.toggle('active');
+
+        if(button.classList.contains('active')){
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+        }else{
+            accordionContent.style.maxHeight = 0;
+        }
+    });
+});
